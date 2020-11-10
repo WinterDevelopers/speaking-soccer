5c19d04553826a3979de1896e6ec9598250a3135
unknown <chrisprogram171041@gmail.com>
2020-11-10 14:02:37 +0100

    hey

--- speaking_soccer/settings.py
+++ speaking_soccer/settings.py
@@ -28,7 +28,7 @@ MEDIA_ROOT = BASE_DIR/'media'
 SECRET_KEY = '3p)^ki0b9g1fh^(xe1r5766m#ft$20qofsq#g@ls-n6@js22-2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -143,7 +143,7 @@ X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 #import django_heroku
 #django_heroku.settings(locals(), staticfiles=False)
